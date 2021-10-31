ccba6e1ee693a13cba0c2b6b5f1f940e7185a7b4
Pedro Henrique Garcia <pehdfms@gmail.com>
2021-10-31 14:29:55 -0300

    implement division by 0 error

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -84,6 +84,10 @@ public class Interpreter implements Expr.Visitor<Object> {
                         "Operands must be two numbers or two strings.");
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Division by 0.");
+                }
+
                 return (double)left / (double)right;
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
